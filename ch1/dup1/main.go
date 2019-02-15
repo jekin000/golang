6b35da5bd8c3601cd9a3c7a5045b0afd2c144b87
jekin000 <kira_r163@163.com>
2019-02-15 14:08:26 +0800

    dup

---
+++ ch1/dup1/main.go
@@ -0,0 +1,22 @@
+//Dup1 prints the text of each line that appears more than
+//once in the standard input, preceded by its count.
+package main
+
+import(
+    "os"
+    "fmt"
+    "bufio"
+)
+
+
+func main(){
+    counts := make(map[string]int)
+    input := bufio.NewScanner(os.Stdin)
+    for input.Scan(){
+        counts[input.Text()]++
+    }
+
+    for line,n := range counts{
+        fmt.Printf("%d\t%s\n",n,line)
+    }
+}
---
+++ ch1/dup3/main.go
@@ -0,0 +1,33 @@
+//Dup2 prints the text of each line that appears more than
+//once in the stdin or file, preceded by its count.
+package main
+
+import(
+    "os"
+    "fmt"
+    "io/ioutil"
+    "strings"
+)
+
+
+func main(){
+    counts := make(map[string]int)
+    for _,filename := range os.Args[1:]{
+        data,err := ioutil.ReadFile(filename)
+        if err != nil{
+            fmt.Fprintf(os.Stderr,"dup3:%v\n",err)
+            continue
+        }
+
+        for _,line := range strings.Split(string(data),"\n"){
+            counts[line]++
+        }
+    }
+
+    for line,n := range counts{
+        if n > 1{
+            fmt.Printf("%d\t%s\n",n,line)
+        }
+    }
+}
+
