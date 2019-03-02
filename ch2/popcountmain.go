facaa06da7bf474cec78603cc05407908e98fa99
jekin000 <kira_r163@163.com>
2019-03-02 09:31:00 +0000

    exe 2.3 added performance calc

--- ch2/popcountmain.go
+++ ch2/popcountmain.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
@@ -14,7 +15,17 @@ func main() {
 			fmt.Fprintf(os.Stderr, "popcountmain %v\n", err)
 			os.Exit(1)
 		}
-		fmt.Printf("direct:%d has %d's 1bits\n", t, popcount.PopCount(t))
-		fmt.Printf(" cycle:%d has %d's 1bits\n", t, popcount.PopCount_Cycle(t))
+		t1 := time.Now()
+		count1 := popcount.PopCount(t)
+		elapsed1 := time.Since(t1)
+
+		t2 := time.Now()
+		count2 := popcount.PopCount_Cycle(t)
+		elapsed2 := time.Since(t2)
+		fmt.Printf("direct:%d has %d's 1bits; elapsed=%s\n", t,count1,elapsed1)
+		fmt.Printf(" cycle:%d has %d's 1bits; elapsed=%s\n", t,count2,elapsed2)
+		//go run popcountmain.go 123
+		//direct:123 has 6's 1bits; elapsed=176ns
+ 		// cycle:123 has 6's 1bits; elapsed=89ns
 	}
 }
