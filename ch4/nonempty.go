8800911666da6d1078ab1840d9cb22a23a08ebbd
jekin000 <kira_r163@163.com>
2019-03-15 16:24:13 +0800

    just a test for commit script

--- ch4/nonempty.go
+++ ch4/nonempty.go
@@ -1,2 +1,38 @@
 //slice memmory skill
 package main
+
+import "fmt"
+
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+
+	return strings[:i]
+}
+
+func nonempty2(strings []string) []string {
+	out := strings[:0]
+	for _, s := range strings {
+		if s != "" {
+			out = append(out, s)
+		}
+	}
+	return out
+}
+
+func main() {
+	data := []string{"one", "", "three"}
+	fmt.Printf("%q\n", nonempty(data))
+	fmt.Printf("%q\n", data)
+
+	//Normally we use data = nonempty(data) to prevent different base data reference
+
+	data1 := []string{"one", "", "three"}
+	data1 = nonempty2(data1)
+	fmt.Printf("%q\n", data1)
+}
